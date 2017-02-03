820602f913a2cd3ef6a2f35193aa068b8625d46f
Nihanshu Purohit <nihanshupurohit@gmail.com>
2017-02-03 00:29:23 -0500

    sequelize setup

--- app/app.js
+++ app/app.js
@@ -12,13 +12,17 @@ var connection = mysql.createConnection({
   password: '7iPcSpiILT'
 })
 
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize('sql9.freemysqlhosting.net', 'sql9156962', '7iPcSpiILT');
+
 connection.connect()
 
-connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
+// connection sql queries are suppose to be written here (if needed). Example of writing a query:
+/*connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
   if (err) throw err
 
   console.log('The solution is: ', rows[0].solution)
-})
+})*/
 
 connection.end()
 
