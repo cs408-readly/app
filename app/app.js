05a8ead9c57ee622ad15714e388a54b08b252bcf
Nihanshu Purohit <nihanshupurohit@gmail.com>
2017-02-03 00:18:32 -0500

    mysql setup

--- app/app.js
+++ app/app.js
@@ -5,6 +5,24 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
+var mysql = require('mysql')
+var connection = mysql.createConnection({
+  host: 'sql9.freemysqlhosting.net',
+  user: 'sql9156962',
+  password: '7iPcSpiILT'
+})
+
+connection.connect()
+
+connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
+  if (err) throw err
+
+  console.log('The solution is: ', rows[0].solution)
+})
+
+connection.end()
+
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
