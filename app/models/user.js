7955df32479dc959f2ea8beeccadb94837ab0192
Nihanshu Purohit <nihanshupurohit@gmail.com>
2017-02-08 01:26:51 -0500

    local-login/signup implemented

--- app/models/user.js
+++ app/models/user.js
@@ -1,5 +1,35 @@
-var User = sequelize.define('user', {
-  email: Sequelize.STRING,
-  firstName: Sequelize.STRING,
-  lastName: Sequelize.STRING
+var mongoose = require('mongoose');
+var bcrypt   = require('bcrypt-nodejs');
+
+var userSchema = mongoose.Schema({
+
+    local            : {
+        firstName    : String,
+        lastName     : String,
+        email        : String,
+        password     : String
+    },
+    facebook         : {
+        id           : String,
+        token        : String,
+        email        : String,
+        name         : String
+    },
+    google           : {
+        id           : String,
+        token        : String,
+        email        : String,
+        name         : String
+    }
+
 });
+
+userSchema.methods.generateHash = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+userSchema.methods.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.local.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
